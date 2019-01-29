422931263b5557ee19d4a446f82e8983f76e2974
jovan <jovan.toskic@investintech.com>
2019-01-29 11:34:45 +0100

    Setting up the registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="register-container">
+        <form @submit.prevent="register">
+            <h2>Register</h2>
+            <div class="email">
+                <label for="email">Email</label>
+                <input id="email" type="text" name="email" v-model="email">
+            </div>
+
+            <div class="password">
+                <label for="password">Password</label>
+                <input id="password" type="password" name="password" v-model="password">
+            </div>
+            <button type="submit">Register</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    import AuthenticationService from '../services/AuthenticationService'
+    // import axios from 'axios'
+    export default {
+        name:'Register',
+        data() {
+            return {
+                email:'',
+                password:''
+            }
+        },
+        methods: {
+            async register() {
+                const response = await AuthenticationService.register({
+                    email:this.email,
+                    password:this.password
+                })
+                console.log('response::',response.data)
+            }
+        }
+    }
+</script>
+
--- server/src/app.js
+++ server/src/app.js
@@ -10,8 +10,8 @@ app.use(bodyParser.json())
 
 app.listen(process.env.PORT || 8081)
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message:'Hello'
+        message:'Your user has been register!'
     })
 })
